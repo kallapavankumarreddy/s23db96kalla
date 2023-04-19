ac6292528bb82cf10c624397b23ee13017791b85
kallapavankumarreddy <kallapavanreddy23@gmail.com>
2023-04-19 00:28:01 -0500

    part 5

--- controllers/pen.js
+++ controllers/pen.js
@@ -121,3 +121,17 @@ exports.pen_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
     };
+
+    // Handle building the view for creating a pen.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.pen_create_Page = function(req, res) {
+console.log("create view")
+try{
+res.render('pencreate', { title: 'pen Create'});
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
--- routes/pen.js
+++ routes/pen.js
@@ -5,4 +5,6 @@ var router = express.Router();
 router.get('/', pen_controlers.pen_view_all_Page );
 /* GET detail pen page */
 router.get('/detail', pen_controlers.pen_view_one_Page);
+/* GET create pen page */
+router.get('/create', pen_controlers.pen_create_Page);
 module.exports = router;
