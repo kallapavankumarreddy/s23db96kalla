3b8354ec3dbf483b349cff22bb574562e3fba999
kallapavankumarreddy <kallapavanreddy23@gmail.com>
2023-04-27 02:24:18 -0500

    done

--- models/pen.js
+++ models/pen.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const penSchema = mongoose.Schema({
-brand: String,
-price: Number,
-color: String
+    brand: {type:String, minlength:2,maxlength:30},
+    price: {type:Number, min:2,max:800},
+    color: {type:String, minlength:1,maxlength:50}
 })
 module.exports = mongoose.model("pen",penSchema)
--- routes/pen.js
+++ routes/pen.js
@@ -3,13 +3,20 @@ const pen_controlers= require('../controllers/pen');
 var router = express.Router();
 /* GET pen */
 router.get('/', pen_controlers.pen_view_all_Page );
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 /* GET detail pen page */
 router.get('/detail', pen_controlers.pen_view_one_Page);
 /* GET create pen page */
-router.get('/create', pen_controlers.pen_create_Page);
+router.get('/create', secured, pen_controlers.pen_create_Page);
 /* GET create update page */
 router.get('/update', pen_controlers.pen_update_Page);
 /* GET delete pen page */
-router.get('/delete', pen_controlers.pen_delete_Page);
+router.get('/delete', secured, pen_controlers.pen_delete_Page);
 
 module.exports = router;
